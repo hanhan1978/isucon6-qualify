c417c06eab55f5a5a1f4558b6b35b877c9abbdde
hanhan1978 <ryo.tomidokoro@gmail.com>
2024-12-05 14:16:28 +0900

    image build

---
+++ cmd/isupam/go.mod
@@ -0,0 +1,3 @@
+module isupam
+
+go 1.23.4
--- tools/build_isupam.sh
+++ tools/build_isupam.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/usr/bin/bash
 OS_TARGETS="linux darwin freebsd windows"
+
+cd ./cmd/isupam
 
 for os in $OS_TARGETS; do
   if [ "$os" == "windows" ]; then
-    GOOS=$os go build -ldflags "-s -w" -o bin/isupam_$os.exe ./cmd/isupam
+    GOOS=$os go build -ldflags "-s -w" -o ../../bin/isupam_$os.exe
   else
-    GOOS=$os go build -ldflags "-s -w" -o bin/isupam_$os ./cmd/isupam
+    GOOS=$os go build -ldflags "-s -w" -o ../../bin/isupam_$os
   fi
 done
